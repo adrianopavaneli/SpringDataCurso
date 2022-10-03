def46cc0c55bd4713c01725965725a546010b0b5
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-10-03 19:43:50 -0300

    Query de busca por nome

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -10,27 +10,25 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatoriosService;
 
 @EnableJpaRepositories
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
 	private Boolean system = true;
-
 	private final CrudCargoService cargoService;
-
-
-
 	private final CrudFuncionarioService funcionarioService;
-
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+	private final RelatoriosService relatorioService;
 
 	public SpringDataApplication(CrudCargoService cargoService,
 			CrudFuncionarioService funcionarioService, 
-			CrudUnidadeTrabalhoService unidadeTrabalhoService) {
+			CrudUnidadeTrabalhoService unidadeTrabalhoService, RelatoriosService relatorioService) {
 		this.cargoService = cargoService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.relatorioService = relatorioService;
 	}
 
 	public static void main(String[] args) {
@@ -47,6 +45,7 @@ public class SpringDataApplication implements CommandLineRunner {
 			System.out.println("1 - Funcionario");
 			System.out.println("2 - Cargo");
 			System.out.println("3 - Unidade");
+			System.out.println("4 - Relatorios");
 			
 			Integer function = scanner.nextInt();
 
@@ -60,6 +59,9 @@ public class SpringDataApplication implements CommandLineRunner {
 					break;
 				case 3:
 					unidadeTrabalhoService.inicial(scanner);
+					break;
+				case 4:
+					relatorioService.inicial(scanner);
 					break;
 				default:
 					System.out.println("Finalizando");
